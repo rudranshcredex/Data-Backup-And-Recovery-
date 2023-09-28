7cb09d5b9d16145045cf4a34d68532b38da29b34
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-09-28 22:53:48 +0530

    org retrieve only useful components included

--- force-app/main/default/lwc/cloudComponent/cloudComponent.js
+++ force-app/main/default/lwc/cloudComponent/cloudComponent.js
@@ -1,6 +1,3 @@
 import { LightningElement } from 'lwc';
-import cloud from '@salesforce/resourceUrl/cloud';
-import cloud from '@salesforce/resourceUrl/';
-
 
 export default class CloudComponent extends LightningElement {}
