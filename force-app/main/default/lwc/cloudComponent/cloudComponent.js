8b73076eca10bdd74dd3709c1dd235dc23173d75
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-10-03 12:36:15 +0530

    added new cloud component

--- force-app/main/default/lwc/cloudComponent/cloudComponent.js
+++ force-app/main/default/lwc/cloudComponent/cloudComponent.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import local from '@salesforce/resourceUrl/LocalBackup';
+import cloud from '@salesforce/resourceUrl/cloud';
 
-export default class CloudComponent extends LightningElement {}
+    
+export default class CloudComponent extends LightningElement {
+    local = local;
+    cloud = cloud;
+    
+}
