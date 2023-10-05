6fc85182db1c4b25209712818b2696dbe9cccc20
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-10-05 18:03:52 +0530

    added user guide and product manual

--- force-app/main/default/lwc/dataRecoveryScreen/dataRecoveryScreen.js
+++ force-app/main/default/lwc/dataRecoveryScreen/dataRecoveryScreen.js
@@ -19,8 +19,8 @@ export default class DataRecoveryScreen extends LightningElement {
 
   currentPageTable1 = 1;
   currentPageTable2 = 1;
-  recordsPerPage = 5; 
-  
+  recordsPerPage = 5;
+
   recoveryAwsText = "Your data recovery from AWS is now in the queue and will be processed shortly. We're working diligently to ensure that your requested data is recovered accurately and securely.";
   lowertext = "You will receive an email notification once the recovery is completed, along with a custom notification .";
   recoveryLocaltext = "Your data recovery is now in the queue and will be processed shortly. We're working diligently to ensure that your requested data is recovered accurately and securely.";
@@ -30,6 +30,9 @@ export default class DataRecoveryScreen extends LightningElement {
   selectedId;
   LocalRecovery = false;
   AwsRecovery = false;
+  isInserted = true;
+  isUpserted = true;
+  bothInsertUpsert = false;
 
   currentStep = "1";
   step = 1;
@@ -187,7 +190,7 @@ export default class DataRecoveryScreen extends LightningElement {
   }
 
   handleRecovery(event) {
-    let divId =event.currentTarget.getAttribute('data-id');
+    let divId = event.currentTarget.getAttribute('data-id');
     console.log(divId);
     console.log('handle recovery');
     if (this.file == null) {
@@ -197,113 +200,110 @@ export default class DataRecoveryScreen extends LightningElement {
     }
     this.retrievalLoading = true;
 
-  if(divId=='Local'){
-    const fileReader = new FileReader();
-    fileReader.onload = () => {
-      const file = fileReader.result.split(',')[1];
-      console.log('files');
-      console.log(file);
+    if (divId == 'Local') {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        const file = fileReader.result.split(',')[1];
+        console.log('files');
+        console.log(file);
 
-      uploadZipFile({ FileData: file })
-        .then(data => {
-          this.retrievalLoading = false;
-          this.objectScreen = true;
-          this.showScreen1 = false;
-          this.step = 2;
-          this.handleStepUp();
-          console.log('data');
-          console.log(data);
-          console.log(JSON.stringify(data));
-          for (var key in data) {
+        uploadZipFile({ FileData: file })
+          .then(data => {
+            this.retrievalLoading = false;
+            this.objectScreen = true;
+            this.showScreen1 = false;
+            this.step = 2;
+            this.handleStepUp();
+            console.log('data');
+            console.log(data);
+            console.log(JSON.stringify(data));
+            for (var key in data) {
 
-            let objwithoutExternalId = {};
-            let objwithExternalId = {};
+              let objwithoutExternalId = {};
+              let objwithExternalId = {};
 
-            console.log(this.objectsWithoutExternalId.length);
-            console.log(data[key].objectName);
-            if (data[key].ExternalIdField.length > 0) {
-              console.log('yes');
-              var externalId = data[key].ExternalIdField;
+              console.log(this.objectsWithoutExternalId.length);
+              console.log(data[key].objectName);
+              if (data[key].ExternalIdField.length > 0) {
+                console.log('yes');
+                var externalId = data[key].ExternalIdField;
 
-              if (externalId[0] == '') {
-                console.log('blank');
-                console.log(data[key].objectName);
-                objwithoutExternalId.objectName = data[key].objectName;
-                objwithoutExternalId.CsvData = data[key].csvData;
-                this.objectsWithoutExternalId.push(objwithoutExternalId);
-                
-              }
-              else {
-                console.log('not blank');
-                console.log(data[key].objectName);
-                objwithExternalId.objectName = data[key].objectName;
-                objwithExternalId.CsvData = data[key].csvData;
-                objwithExternalId.ExternalIds = data[key].ExternalIdField;
-                this.objectsWithExternalId.push(objwithExternalId);
+                if (externalId[0] == '') {
+                  objwithoutExternalId.objectName = data[key].objectName;
+                  objwithoutExternalId.CsvData = data[key].csvData;
+                  this.objectsWithoutExternalId.push(objwithoutExternalId);
+                  console.log('blank');
+                }
+                else {
+                  console.log('not blank');
+                  objwithExternalId.objectName = data[key].objectName;
+                  objwithExternalId.CsvData = data[key].csvData;
+                  objwithExternalId.ExternalIds = data[key].ExternalIdField;
+                  this.objectsWithExternalId.push(objwithExternalId);
+                }
               }
             }
-          }
-            console.log('without');
-            console.log(this.objectsWithoutExternalId);
-            console.log(this.objectsWithoutExternalId.length);
-            console.log('with');
-            console.log(this.objectsWithExternalId);
-            console.log(this.objectsWithExternalId.length);
-            if (this.objectsWithoutExternalId.length === 0) {
-              this.tableWithoutId = false;
-              console.log('this.objectsWithoutExternalId', this.objectsWithoutExternalId, this.objectsWithoutExternalId.length);
+              if (this.objectsWithoutExternalId.length === 0) {
+                this.tableWithoutId = false;
+                console.log('this.objectsWithoutExternalId', this.objectsWithoutExternalId, this.objectsWithoutExternalId.length);
 
-            }
-            if (this.objectsWithExternalId.length === 0) {
-              console.log('this.objectsWithExternalId', this.objectsWithExternalId, this.objectsWithExternalId.length);
-              this.tableWithId = false;
-            }
-            if (this.objectsWithExternalId.length > 0) {
-              console.log('greater than zero');
-              this.tableWithId = true;
-              console.log(this.tableWithId);
-            }
-            console.log('after call');
+              }
+              if (this.objectsWithExternalId.length === 0) {
+                console.log('this.objectsWithExternalId', this.objectsWithExternalId, this.objectsWithExternalId.length);
+                this.tableWithId = false;
+              }
+              if (this.objectsWithExternalId.length > 0) {
+                console.log('greater than zero');
+                this.tableWithId = true;
+                console.log(this.tableWithId);
+              }
+              console.log('after call');
+
+
+            /*   console.log('objwithoutExternalId      ');
+               console.log(objwithoutExternalId);
+               console.log(this.objwithoutExternalId.length);*/
+   
+             //  this.objectsWithoutExternalId.push(objwithoutExternalId);
+               console.log('without ');
+               console.log(this.objectsWithoutExternalId);
+               console.log(this.objectsWithoutExternalId.length);
+               console.log(this.objectsWithoutExternalId);
+
+              //console.log(JSON.stringify(this.objectsWithExternalId));
+              // if (this.objectsWithExternalId.length == 0 ) {
+              //   console.log('objectsWithExternalId', this.objectsWithExternalId.length, JSON.stringify(this.objectsWithExternalId) );
+              //     this.tableWithId = false;
+              // }
+              // console.log('without ids');
+              // console.log('length', this.objectsWithoutExternalId.length);
+              // console.log(JSON.stringify(this.objectsWithoutExternalId));
+              // if (this.objectsWithoutExternalId.length == 0 ) {
+              //   console.log('objectsWithoutExternalId', this.objectsWithoutExternalId.length, JSON.stringify(this.objectsWithoutExternalId));
+              //     this.tableWithoutId = false;
+              // }
+             /* console.log(data[key].ExternalIdField);
+              console.log(key);*/
+              console.log('bothInsertUpsert before', this.bothInsertUpsert);
             
-            console.log(this.objectsWithoutExternalId.length);
-            console.log(this.objwithExternalId.length);
+            if (this.objectsWithoutExternalId.length > 0 && this.objectsWithExternalId.length > 0) {
+              this.bothInsertUpsert = true;
+              console.log('bothInsertUpsert after', this.bothInsertUpsert);
+            }
+         /*   else {
+              this.bothInsertUpsert = false;
+            }*/
+          })
+          .catch(error => {
+            console.log('inside error');
+            console.log('error', error);
+          })
 
-            /* console.log('objwithoutExternalId      ');
-             console.log(objwithoutExternalId);
-             console.log(objwithoutExternalId.length);
- 
-             this.objectsWithoutExternalId.push(objwithoutExternalId);
-             console.log('without ');
-             console.log(this.objectsWithoutExternalId);
-             console.log(this.objectsWithoutExternalId.length);
-             console.log(objectsWithoutExternalId);*/
+      };
+      fileReader.readAsDataURL(this.file);
+    }
 
-            //console.log(JSON.stringify(this.objectsWithExternalId));
-            // if (this.objectsWithExternalId.length == 0 ) {
-            //   console.log('objectsWithExternalId', this.objectsWithExternalId.length, JSON.stringify(this.objectsWithExternalId) );
-            //     this.tableWithId = false;
-            // }
-            // console.log('without ids');
-            // console.log('length', this.objectsWithoutExternalId.length);
-            // console.log(JSON.stringify(this.objectsWithoutExternalId));
-            // if (this.objectsWithoutExternalId.length == 0 ) {
-            //   console.log('objectsWithoutExternalId', this.objectsWithoutExternalId.length, JSON.stringify(this.objectsWithoutExternalId));
-            //     this.tableWithoutId = false;
-            // }
-            console.log(data[key].ExternalIdField);
-            console.log(key);
-          
-        })
-        .catch(error => {
-          console.log('inside error');
-          console.log('error', error);
-        })
-
-    };
-    fileReader.readAsDataURL(this.file);
-  }
-
-    if(divId=='Aws'){
+    if (divId == 'Aws') {
       uploadZipFile({ FileData: this.file })
         .then(data => {
           this.retrievalLoading = false;
@@ -353,7 +353,6 @@ export default class DataRecoveryScreen extends LightningElement {
               this.tableWithId = true;
               console.log(this.tableWithId);
             }
-            
             console.log('after call');
 
 
@@ -391,10 +390,13 @@ export default class DataRecoveryScreen extends LightningElement {
   }
 
   handleInsert() {
+    this.isInserted = false;
     this.showScreen2 = true;
-    this.objectScreen = false;
+    // this.objectScreen = false;
     this.step = 3;
     this.handleStepUp();
+
+    this.showToast('Success', 'Insertion in progress', 'success');
 
     /*if (buttonId === 'btn-1') {
         this.RecoveryLocalScreen = true;
@@ -444,17 +446,22 @@ export default class DataRecoveryScreen extends LightningElement {
 
     }
     else {
+      this.isUpserted = false;
+      this.RecoveryLocalScreen = true;
+      // this.objectScreen = false;
       performUpsert({ objectsToUpsert: JSON.stringify(this.selectedObjectDataString) })
         .then(data => {
           console.log('data');
           console.log(data);
-          this.showToast('Data Upserted', 'Move to the next Step', 'success');
+
 
         })
         .catch(error => {
           console.log(error);
         })
+      this.showToast('Success', 'Upsertion in progress', 'success');
     }
+
   }
   showToast(title, message, variant) {
     this.dispatchEvent(
@@ -472,53 +479,49 @@ export default class DataRecoveryScreen extends LightningElement {
     this.currentStep = "" + this.step;
   }
 
-
-  
-
-
-handleNextPageTable1() {
+  handleNextPageTable1() {
     if (this.currentPageTable1 < this.totalPagesTable1) {
-        this.currentPageTable1++;
+      this.currentPageTable1++;
     }
-}
+  }
 
-handlePreviousPageTable1() {
+  handlePreviousPageTable1() {
     if (this.currentPageTable1 > 1) {
-        this.currentPageTable1--;
+      this.currentPageTable1--;
     }
-}
+  }
 
-handleNextPageTable2() {
+  handleNextPageTable2() {
     if (this.currentPageTable2 < this.totalPagesTable2) {
-        this.currentPageTable2++;
+      this.currentPageTable2++;
     }
-}
+  }
 
-handlePreviousPageTable2() {
+  handlePreviousPageTable2() {
     if (this.currentPageTable2 > 1) {
-        this.currentPageTable2--;
+      this.currentPageTable2--;
     }
-}
+  }
 
-get totalPagesTable1() {
+  get totalPagesTable1() {
     return Math.ceil(this.objectsWithoutExternalId.length / this.recordsPerPage);
-}
+  }
 
-get totalPagesTable2() {
+  get totalPagesTable2() {
     return Math.ceil(this.objectsWithExternalId.length / this.recordsPerPage);
-}
+  }
 
-get displayedDataTable1() {
+  get displayedDataTable1() {
     const startIndex = (this.currentPageTable1 - 1) * this.recordsPerPage;
     const endIndex = startIndex + this.recordsPerPage;
     return this.objectsWithoutExternalId.slice(startIndex, endIndex);
-}
+  }
 
-get displayedDataTable2() {
+  get displayedDataTable2() {
     const startIndex = (this.currentPageTable2 - 1) * this.recordsPerPage;
     const endIndex = startIndex + this.recordsPerPage;
     return this.objectsWithExternalId.slice(startIndex, endIndex);
-}
+  }
 
 
 
